018002ffb094c7462fd83fd15f992c4a7556f7e3
khrystynaklochko <khrystynaklochko@gmail.com>
2018-01-22 01:20:52 +0100

    initial commit

---
+++ lib/tweets_filter_elixir.ex
@@ -0,0 +1,18 @@
+defmodule TweetsFilterElixir do
+  @moduledoc """
+  Documentation for TweetsFilterElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TweetsFilterElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tweets_filter_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule TweetsFilterElixirTest do
+  use ExUnit.Case
+  doctest TweetsFilterElixir
+
+  test "greets the world" do
+    assert TweetsFilterElixir.hello() == :world
+  end
+end
